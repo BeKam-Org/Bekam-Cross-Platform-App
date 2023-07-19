75f03d472eb093ef9a93e48a4d9df217c91913cc
Mohamed-7018 <mohamedkhalilalmasri@gmail.com>
2023-07-19 04:35:57 +0200

    them modes (selected + system if null)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bekam/core/theme/dark_theme.dart';
 import 'package:bekam/core/theme/light_theme.dart';
 import 'package:bekam/data/cubit/on_boarding_cubit.dart';
+import 'package:bekam/data/cubit/theme_cubit.dart';
 import 'package:bekam/presentation/screens/onboarding/on_boarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -17,18 +18,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
+        BlocProvider<ThemeCubit>(
+          create: (context) => ThemeCubit(),
+        ),
         BlocProvider<OnBoardingCubit>(
-          create: (context)=> OnBoardingCubit(),
+          create: (context) => OnBoardingCubit(),
         ),
       ],
       child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: lightTheme,
-          debugShowCheckedModeBanner: false,
-          darkTheme: darkTheme,
-          themeMode: ThemeMode.light,
-          home: const OnBoardingScreen(),
-        ),
+        title: 'Flutter Demo',
+        theme: lightTheme,
+        debugShowCheckedModeBanner: false,
+        darkTheme: darkTheme,
+        themeMode:
+            !ThemeCubit.get(context).theme ? ThemeMode.dark : ThemeMode.light,
+        home: const OnBoardingScreen(),
+      ),
     );
   }
 }
