f991ee51518c9a603a2da1a667e36ab09ba8a1ae
Mohamed Samir Salem <mohamedkhalilalmasri@gmail.com>
2023-11-21 07:18:24 +0200

    latest product repo & cubit

---
+++ lib/data/business_logic/latest_product/latest_product_get_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:bekam/data/business_logic/latest_product/latest_product_get_state.dart';
+import 'package:bekam/data/model/network_exception/network_exception.dart';
+import 'package:bekam/data/model/product_model/product_model.dart';
+import 'package:bekam/data/repository/latest_product_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// A [Cubit] responsible for managing the state of Latest Product data fetching.
+///
+/// This Cubit communicates with the UI to provide different states like [Idle], [Loading],
+/// [Success], and [Error] based on the asynchronous data fetching operation.
+class LatestProductGetCubit extends Cubit<LatestProductGetState<ProductModel>> {
+  /// The repository responsible for handling Latest products-related data operations.
+  final LatestProductRepository productRepository;
+
+  /// Constructor for [LatestProductGetCubit] that takes an instance of [LatestProductRepository].
+  LatestProductGetCubit(this.productRepository) : super(const Idle());
+
+  /// Fetches the Latest Product product model and updates the state accordingly.
+  ///
+  /// On successful fetching, emits [LatestProductGetState.success] with the list of [Product].
+  /// On failure, emits [LatestProductGetState.error] with [NetworkExceptions] details.
+  void getLatestProductList(
+      {required int limit, required String offset}) async {
+    // Fetch latest product product model from the repository.
+    var data = await productRepository.getLatestProductList(
+        limit: limit, offset: offset);
+
+    // Process the result and emit the corresponding state.
+    data.when(success: (ProductModel searchResult) {
+      emit(LatestProductGetState.success(searchResult));
+    }, failure: (NetworkExceptions networkExceptions) {
+      emit(LatestProductGetState.error(networkExceptions));
+    });
+  }
+}
---
+++ lib/data/business_logic/latest_product/latest_product_get_state.dart
@@ -0,0 +1,23 @@
+import 'package:bekam/data/model/network_exception/network_exception.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'latest_product_get_state.freezed.dart';
+
+/// A union class using Freezed to represent different states of latest product data fetching.
+///
+/// The states include [Idle], [Loading], [Success], and [Error].
+@freezed
+class LatestProductGetState<T> with _$LatestProductGetState {
+  /// Represents the idle state where no data fetching operation is in progress.
+  const factory LatestProductGetState.idle() = Idle<T>;
+
+  /// Represents the loading state indicating an ongoing data fetching operation.
+  const factory LatestProductGetState.loading() = Loading<T>;
+
+  /// Represents the success state with the fetched data of type [T].
+  const factory LatestProductGetState.success(T data) = Success<T>;
+
+  /// Represents the error state with details of the occurred exception.
+  const factory LatestProductGetState.error(NetworkExceptions networkExceptions) =
+      Error<T>;
+}
